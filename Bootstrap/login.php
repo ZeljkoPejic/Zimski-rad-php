9ace4d62a84d8dd478e599d2560edec2ba11b121
Zeljko Pejic <zeljkopejic@yahoo.com>
2021-01-04 19:52:04 +0100

    gotov zadatak u bootstrap-u

--- Bootstrap/login.php
+++ Bootstrap/login.php
@@ -5,20 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <style>
-        #centriranje{
-            text-align: center;
-            align-content: center ;
-        }
-    </style>
-</head>
+    </head>
 <body>
 
 <?php include_once 'izbornik.php' ?>
 
 <div class="container">
-  <p id="centriranje">Dobrodošli na stranicu</p>
-  <a href="login.php" id="centriranje">Kliknite za logiranje</a>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Korisnički račun</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  <label for="inputPassword" class="col-sm-2 col-form-label">Lozinka</label>
+  <input type="password" class="form-control" id="inputPassword">
+  <a href="nadzornaploca.php">Ulogirajte se</a>
+</div>
+
 </div>
 
 
---
+++ Bootstrap/nadzornaploca.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    </head>
+<body>
+
+<?php include_once 'izbornik.php' ?>
+
+<div class="container">
+  <p style="text-align: center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porttitor suscipit lacus eget blandit. 
+              Nunc eu sagittis tortor. Duis porttitor ex ipsum, in egestas nulla tempus vitae. Nam euismod condimentum diam
+              ut blandit. Aliquam malesuada metus et neque malesuada, et porttitor libero egestas. Nam sed dolor eget tellus 
+              porta tempus vel sed augue. Cras mollis sodales ante, in elementum nibh tincidunt quis. Sed eleifend pulvinar 
+              tortor non aliquam. Suspendisse arcu turpis, facilisis in vulputate eu, iaculis vel leo. Aliquam felis ante, 
+              varius vitae odio eu, elementum auctor metus. Proin tempus odio ac ligula gravida imperdiet. Aenean a pretium
+              neque. Cras et enim eu nisi egestas congue. Suspendisse ac tincidunt nunc, lacinia fermentum arcu. Aenean sed
+              nisl sapien. Aenean tincidunt mi ac eros vulputate, in lacinia urna fermentum. Etiam sem libero, rhoncus ac
+              arcu sit amet, mattis luctus nisi. Integer eget dignissim ligula, id dapibus nunc. Vestibulum finibus felis
+              nec erat cursus, commodo lacinia sapien volutpat. In congue tincidunt turpis. Nullam vitae elit interdum,
+              tempor turpis semper, vehicula massa. Phasellus rutrum pellentesque magna, at molestie tellus varius in. 
+              Suspendisse quis laoreet massa. Nullam ac leo bibendum, blandit lorem ac, vehicula ante. Aliquam egestas 
+              bibendum elit sagittis ultrices. Vivamus nunc sem, ornare sit amet diam et, gravida tempor est. Lorem 
+              ipsum dolor sit amet, consectetur adipiscing elit. Etiam vestibulum, nibh non interdum faucibus, massa 
+              ligula iaculis tortor, eu scelerisque eros dolor sed nulla. Nulla ornare ultricies turpis id laoreet. 
+              Ut consequat dolor eget orci accumsan, eu tempus ex facilisis. Suspendisse potenti. Vestibulum turpis 
+              leo, bibendum id leo et, vestibulum dignissim sapien. Duis turpis est, pellentesque quis lorem et, dapibus 
+              tempor lacus. Sed id ligula sit amet urna volutpat aliquet ac ut diam. Nullam sed ex sapien. Etiam eleifend,
+              lorem eu pellentesque aliquam, justo dui convallis magna, sit amet placerat nibh libero non erat. In hac 
+              habitasse platea dictumst. Cras commodo ut ex vel efficitur. Vestibulum ac est aliquam, tempor nisl et, 
+              luctus mauris. Quisque velit nunc, sodales nec libero non, vestibulum consequat nunc. Pellentesque tristique 
+              ullamcorper elit in viverra.Praesent fermentum hendrerit sem, et rutrum urna auctor sit amet. In condimentum
+              ex in viverra mollis. Curabitur pretium tortor ante, id consectetur sem lacinia congue. Praesent sed ante
+              fermentum, congue dolor id, fringilla felis. Phasellus vitae nunc sed risus sollicitudin blandit in sit 
+              amet eros. Donec sagittis, ex quis luctus tristique, tellus quam vestibulum nisi, ut tincidunt dui libero
+              et nunc. Phasellus vulputate enim non ipsum vulputate eleifend. Pellentesque lobortis pulvinar eros quis
+              sodales. Integer ultricies faucibus purus at ornare. Integer venenatis porta hendrerit. Integer suscipit
+              diam lobortis nibh consequat, eu venenatis arcu interdum.
+    </p>
+</div>
+
+
+
+<?php include_once 'skripte.php' ?>
+
+    
+</body>
+</html>
