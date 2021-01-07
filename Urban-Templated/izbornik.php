fa76e769aacb3efd6e19dfd2e371dc5c4b5e19b5
Zeljko Pejic <zeljkopejic@yahoo.com>
2021-01-07 18:15:50 +0100

    zapocet zadatak u besplatnome template-u (Urban)

---
+++ Urban-Templated/izbornik.php
@@ -0,0 +1,8 @@
+
+<nav id="menu">
+	<ul class="links">
+		<li><a href="index.php">Početna stranica</a></li>
+		<li><a href="nadzornaploca.php">Nadzorna ploča</a></li>
+		<li><a href="login.php">Log in</a></li>
+	</ul>
+</nav>
