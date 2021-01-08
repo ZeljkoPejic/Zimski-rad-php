a6449e2f9bb794a18c4569789ea4eda86396f21a
Zeljko Pejic <zeljkopejic@yahoo.com>
2021-01-08 22:04:40 +0100

    gotova pocetna stranica

--- Urban-Templated/index.php
+++ Urban-Templated/index.php
@@ -34,7 +34,7 @@
 			<div id="main">
 
 				<!-- Section -->
-					<section class="wrapper style1">
+					<section class="wrapper style2">
 						<div class="inner">
 							<!-- 2 Columns -->
 								<div class="flex flex-2">
@@ -54,18 +54,17 @@
 					</section>
 
 				<!-- Section -->
-					<section class="wrapper style2">
+					<section class="wrapper style1">
 						<div class="inner">
 							<div class="flex flex-2">
 								<div class="col col2">
-									<h3>Suspendisse quis massa vel justo</h3>
-									<p>Etiam posuere hendrerit arcu, ac blandit nulla. Sed congue malesuada nibh, a varius odio vehicula aliquet. Aliquam consequat, nunc quis sollicitudin aliquet, enim magna cursus auctor lacinia nunc ex blandit augue. Ut vitae neque fermentum, luctus elit fermentum, porta augue. Nullam ultricies, turpis at fermentum iaculis, nunc justo dictum dui, non aliquet erat nibh non ex.</p>
-									<p>Sed congue malesuada nibh, a varius odio vehicula aliquet. Aliquam consequat, nunc quis sollicitudin aliquet, enim magna cursus auctor lacinia nunc ex blandit augue. Ut vitae neque fermentum, luctus elit fermentum, porta augue. Nullam ultricies, turpis at fermentum iaculis, nunc justo dictum dui, non aliquet erat nibh non ex. </p>
-									
+									<h3>Shakespeare invented more than 1,700 words.</h3>
+									<p>Shakespeare wrote some of the most beloved and revered pieces of literature the world has ever known, but in order to craft his plays and poems, he sometimes resorted to making up his own words.</p>
+									<p> In fact, The Bard is said to have come up with more than 1,700 words including moonbeam, laughable, eyeball, bump, puking, champion, bedroom, excitement, and zany.</p>
 								</div>
 								<div class="col col1 first">
 									<div class="image round fit">
-										<a href="generic.html" class="link"><img src="images/pic02.jpg" alt="" /></a>
+										<a href="generic.html" class="link"><img src="images/shakespeare.jpg" alt="" /></a>
 									</div>
 								</div>
 							</div>
@@ -73,33 +72,74 @@
 					</section>
 
 				<!-- Section -->
+				<section class="wrapper style1">
+						<div class="inner">
+							<div class="flex flex-2">
+								<div class="col col2">
+									<h3>Einstein's brain was stolen when he died.</h3>
+									<p>When Nobel Prize-winning physicist Albert Einstein passed away on April 18, 1955, he left behind specific instructions when it came to the disposal of his body, according to one National Geographic investigation. Einstein didn't want his corpse to be worshiped or his brain to be studied, so he instructed those who were responsible for his remains to "cremate them, and scatter the ashes secretly in order to discourage idolaters."</p>
+									<p>However, Thomas Harvey, the pathologist on call when Einstein died at New Jersey's Princeton Hospital, didn't quite follow those instructions. Instead, he stole Einstein's brain. From there, things got even weirder. When Einstein's family found out, his son apparently didn't object to the theft and Harvey was able to keep the brain in two jars in his basement before moving it to "a cider box stashed under a beer cooler."</p>
+								</div>
+								<div class="col col1 first">
+									<div class="image round fit">
+										<a href="generic.html" class="link"><img src="images/albert-einstein.jpg" alt="" /></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+
+					<!-- Section -->
 					<section class="wrapper style1">
 						<div class="inner">
-							<header class="align-center">
-								<h2>Aliquam ipsum purus dolor</h2>
-								<p>Cras sagittis turpis sit amet est tempus, sit amet consectetur purus tincidunt.</p>
-							</header>
-							<div class="flex flex-3">
-								<div class="col align-center">
-									<div class="image round fit">
-										<img src="images/pic03.jpg" alt="" />
-									</div>
-									<p>Sed congue elit malesuada nibh, a varius odio vehicula aliquet. Aliquam consequat, nunc quis sollicitudin aliquet. </p>
-									<a href="#" class="button">Learn More</a>
+							<div class="flex flex-2">
+								<div class="col col2">
+									<h3>3D printing is nothing new</h3>
+									<p>The technology for 3D printing has been around for decades, but it has only begun to attract attention in the past few years. The concept initially appeared during the 1980s, when it was referred to as Rapid Prototyping. Charles Hull successfully secured a patent for the stereolithography apparatus in 1986, and yet, the first commercially available 3D printer was not offered for sale until 2009.</p>
+									<p>Since then significant market divergence has allowed for the introduction of 3D printing processes in several sectors and at every level of the market.</p>
 								</div>
-								<div class="col align-center">
+								<div class="col col1 first">
 									<div class="image round fit">
-										<img src="images/pic05.jpg" alt="" />
+										<a href="generic.html" class="link"><img src="images/3dprinter.jpg" alt="" /></a>
 									</div>
-									<p>Sed congue elit malesuada nibh, a varius odio vehicula aliquet. Aliquam consequat, nunc quis sollicitudin aliquet. </p>
-									<a href="#" class="button">Learn More</a>
 								</div>
-								<div class="col align-center">
+							</div>
+						</div>
+					</section>
+
+					<!-- Section -->
+					<section class="wrapper style1">
+						<div class="inner">
+							<div class="flex flex-2">
+								<div class="col col2">
+									<h3>In 1956, 5 megabytes (5MB) of data weighed a ton</h3>
+									<p>It was 1956 when IBM launched RAMAC, the first computer with something like a hard drive that we use today.</p>
+									<p>By hard drive, we mean something that used magnetic disks - a moving head was used to access and write that data. At the time, it was considered a massive leap in massstorage technology because it signified a shift: from punch cards and magnetic tape (which stored data sequentially) to randomly accessible hard drives.</p>
+									<p>RAMAC itself stood for Random Access Method of Accounting & Control. The whole cabinet weighed over 1000kg and the 5MP data was spread over 50 huge aluminium disks, coated with magnetic iron oxide. The disks rotated at a speed of 1200rpm and the machines were leased for $3,200 per month back in the day.</p>
+								</div>
+								<div class="col col1 first">
 									<div class="image round fit">
-										<img src="images/pic04.jpg" alt="" />
+										<a href="generic.html" class="link"><img src="images/ibm.jpg" alt="" /></a>
 									</div>
-									<p>Sed congue elit malesuada nibh, a varius odio vehicula aliquet. Aliquam consequat, nunc quis sollicitudin aliquet. </p>
-									<a href="#" class="button">Learn More</a>
+								</div>
+							</div>
+						</div>
+					</section>
+
+					<!-- Section -->
+					<section class="wrapper style1">
+						<div class="inner">
+							<div class="flex flex-2">
+								<div class="col col2">
+									<h3>Did you know your devices listen to you all the time?</h3>
+									<p>Did you know that your Alexa device listens to your conversations, like, all the time?</br>This goes out for Siri as well!</p>
+									<p>According to <a href="https://www.independent.co.uk/life-style/gadgets-and-tech/news/amazon-alexa-echo-listening-spy-security-a8865056.html">The Independent</a>, Alexa has been eavesdropping conversations in order to improve the whole virtual assistant experience. An Amazon spokesman even said in a statement: “We only annotate an extremely small sample of Alexa voice recordings in order to improve the customer experience. Employees do not have direct access to information that can identify the person or account as part of this workflow. All information is treated with high confidentiality and we use multi-factor authentication to restrict access, service encryption and audits of our control environment to protect it.”</p>
+									<p>Regarding Siri, <a href="https://www.foxbusiness.com/technology/apples-siri-is-eavesdropping-on-your-conversations-putting-users-at-risk">Fox Business</a> stated in a report that “Apple’s Siri can be activated by something as mundane as the sound of a zipper — leaving any conversation open to surveillance and accompanied by user data like location and contact details.”</p>
+								</div>
+								<div class="col col1 first">
+									<div class="image round fit">
+										<a href="generic.html" class="link"><img src="images/pic02.jpg" alt="" /></a>
+									</div>
 								</div>
 							</div>
 						</div>
